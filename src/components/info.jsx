479ecb3ff3dd9bc166289b3625b0931c66216de2
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2022-07-24 22:15:56 +0530

    finish info.jsx

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import profile from "../assets/profile image.svg";
+import emailIcon from'../assets/email icon.svg'
+import linkedInIcon from'../assets/linkedIn icon.svg'
 
-export default function Info(){
-    return(
-        <div>
-         <h1>Info goes here!</h1>
-        </div>
-    );
+export default function Info() {
+  return (
+    <div className="info">
+      <div className="profile-image">
+        <img src={profile} alt="profile" />
+      </div>
+      <div>
+        <p className="name">Laura Smith</p>
+        <p className="desig">Frontend Developer</p>
+      </div>
+      <h5 className="website">laurasmith.website</h5>
+      <div className="contact-buttons">
+        <img src={emailIcon} alt="" className="email-icon"/>
+        <img src={linkedInIcon} alt="" className="linkedin-icon"/>
+      </div>
+    </div>
+  );
 }
